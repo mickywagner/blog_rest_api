4f200bde52f934e8be5adfb07796e0499442ff24
Michaela Wagner <mickywagner@gmail.com>
2020-05-24 04:52:24 -0600

    Add cors and mongoose

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import 'dotenv/config'
-
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Future Blog API here')
 })
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on port 3000 ${process.env.PORT}`)
 })
