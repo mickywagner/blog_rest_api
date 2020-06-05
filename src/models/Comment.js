ac232e2ba7dfc27615c0069bb54bdee52f801306
Michaela Wagner <mickywagner@gmail.com>
2020-06-05 17:14:50 -0600

    Add ref to Comment.post

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -7,7 +7,7 @@ const CommentSchema = new Schema(
         name: {type: String, required: true},
         text: {type: String, required: true},
         timestamp: {type: Date, required: true},
-        post: { type: Schema.Types.ObjectId, required: true}
+        post: { type: Schema.Types.ObjectId, required: true, ref: 'BlogPost'}
     }
 )
 
