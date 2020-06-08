c811689275470a2b43187865c2f7743e236d3226
Michaela Wagner <mickywagner@gmail.com>
2020-06-07 19:08:11 -0600

    Connect reader front end to API

--- reader-frontend/src/App.js
+++ reader-frontend/src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const [allPosts, setAllPosts] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/posts')
+      .then(response => response.json())
+      .then(json => {
+        setAllPosts(json)
+        console.log(json)
+        setIsLoading(false)
+      }
+      )
+      .catch(err => console.log(err))
+  }, [])
+
+  const posts = allPosts.map(post => (
+    <div>
+      <h5>{post.title}</h5>
+      <p>{post.text}</p>
+    </div>
+  ))
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Reader Front-end</h1>
+          {isLoading && <p>Blog posts are loading...</p>}
+
+          {allPosts.length !== 0 && posts } 
+          
       </header>
     </div>
   );
