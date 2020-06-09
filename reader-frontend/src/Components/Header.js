346f0e57a33ec96df80ff27d17eee5fab7ac3c76
Michaela Wagner <mickywagner@gmail.com>
2020-06-09 13:57:24 -0600

    Add links in header

--- reader-frontend/src/Components/Header.js
+++ reader-frontend/src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return(
         <div className="app-header">
-            
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/posts">Blogs</Link></li>
+                </ul>
+            </nav>
         </div>
     )
 }
