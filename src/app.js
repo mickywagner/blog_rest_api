a6176e8f34201a65f2811bd7321f85f6cf8f8d61
Michaela Wagner <mickywagner@gmail.com>
2020-06-10 15:21:47 -0600

    Move login to index router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import express from 'express'
 import { connectDb } from './models'
 import indexRouter from './routes/index'
 import apiRouter from './routes/api'
+import authRouter from './routes/auth'
 import { models, model } from 'mongoose'
 import createUserWithPosts from '../populate'
 
@@ -13,8 +14,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
 app.use('/', indexRouter)
 app.use('/api', apiRouter) 
+app.use('/auth', authRouter)
+
 
 connectDb().then(async () => {
 //    createUserWithPosts()
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,38 @@
 import express from 'express'
 const router = express.Router()
 
+import User from '../models/User'
+import jwt from 'jsonwebtoken'
+
 router.get('/', (req, res) => {
     res.redirect('/api')
+})
+
+router.post('/login', (req, res, next) => {
+    let { email, password } = req.body
+
+    User.findOne({email: email}).exec(function(err, user) {
+        if(err || !user) {
+            return res.status(401).json({
+                message: 'Authorization fail'
+            })
+        }
+
+        if(user.password === password) {
+            const userForToken = {
+                username: user.username,
+                id: user._id
+            }
+
+            const accessToken = jwt.sign(userForToken, process.env.ACCESS_TOKEN_SECRET)
+            return res.status(200).json({
+                message: 'Login success',
+                user: userForToken,
+                accessToken: accessToken
+            })
+        }
+    }
+    )
 })
 
 export default router
