b0fcd28ef1c84a27c525bbc43801fff02847bd72
Michaela Wagner <mickywagner@gmail.com>
2020-06-16 15:28:10 -0600

    Export pages from pages/index, add Login, Welcome routes to app, remove css

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Route, Link} from "react-router-dom"
+import {Route} from "react-router-dom"
+import {Login, Welcome} from './Pages/'
 
 function App() {
   return (
     <div className="App">
-      
+        <Route exact path="/">
+          <Login />
+        </Route>
+
+        <Route path="/admin">
+          <Welcome />
+        </Route>
     </div>
   );
 }
