644bac1f2589acd9a4bc9d7f03ef019d1c1e06ad
Michaela Wagner <mickywagner@gmail.com>
2020-06-24 15:11:29 -0600

    Create new blog call to API works

--- admin-frontend/src/Pages/CreateNewPost.js
+++ admin-frontend/src/Pages/CreateNewPost.js
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
-
-import { Editor } from '@tinymce/tinymce-react';
-
+// import { Editor } from '@tinymce/tinymce-react';
 
 function CreateNewPost() {
-    const [content, setContent] = useState('')
 
     const submitBlog = (e) => {
         e.preventDefault()
-        
-    }
+        const {title, content, publish} = e.target
 
-    const handleEditorChange = (content, editor) => {
-        console.log('Content was updated:', content);
-        setContent(content)
-    
+        fetch(`/api/posts`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZTU2M2M1YzY1ZjdmNjQ5NmJhZWIxMyIsInVzZXJuYW1lIjoiU2NhcmxldHQiLCJpYXQiOjE1OTI5NjA3NDQsImV4cCI6MTU5MzA0NzE0NH0.yW86EWszvpiOCglRlsKq6-Zo0_S7A0q17vIP79ik-cI'
+            }, 
+            body: JSON.stringify({
+                "title": title.value,
+                "text": content.value,
+                "isPublished": publish.checked
+            })
+        }).then(res => res.json()).then(data => console.log(data))
     }
 
     return(
@@ -23,25 +27,9 @@ function CreateNewPost() {
             <div className="editor">
                 <form onSubmit={submitBlog}>
                     <input type="text" id="title" placeholder="Blog Title"></input>
-                    <Editor
-                        initialValue="<p>This is the initial content of the editor</p>"
-                        init={{
-                        height: 500,
-                        menubar: false,
-                        plugins: [
-                            'advlist autolink lists link image charmap print preview anchor',
-                            'searchreplace visualblocks code fullscreen',
-                            'insertdatetime media table paste code help wordcount'
-                        ],
-                        toolbar:
-                            'undo redo | formatselect | bold italic backcolor | \
-                            alignleft aligncenter alignright alignjustify | \
-                            bullist numlist outdent indent | removeformat | help'
-                        }}
-                        onEditorChange={handleEditorChange}
-                        value={content}
-                    />
-                    <button>Submit Post</button>
+                    <textarea id="content" placeholder="Blog post text"></textarea>
+                    <label><input type='checkbox' id="publish"></input> Publish Post</label>
+                    <button>Save Post</button>
                 </form>
             </div>
             
@@ -50,3 +38,23 @@ function CreateNewPost() {
 }
 
 export default CreateNewPost
+
+
+{/* <Editor
+initialValue="<p>This is the initial content of the editor</p>"
+init={{
+height: 500,
+menubar: false,
+plugins: [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table paste code help wordcount'
+],
+toolbar:
+    'undo redo | formatselect | bold italic backcolor | \
+    alignleft aligncenter alignright alignjustify | \
+    bullist numlist outdent indent | removeformat | help'
+}}
+onEditorChange={handleEditorChange}
+value={content}
+/> */}
