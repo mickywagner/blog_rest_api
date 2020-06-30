ae2957fd853ad9cf4a3be845776846504df20dac
Michaela Wagner <mickywagner@gmail.com>
2020-06-30 15:30:55 -0600

    Move like/dislike to own component function

--- reader-frontend/src/Components/BlogWithComment.js
+++ reader-frontend/src/Components/BlogWithComment.js
@@ -1,54 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+
 import Comment from './Comment'
 import CommentForm from './CommentForm'
-import { FaHeart, FaHeartBroken } from 'react-icons/fa'
-import { normalizeUnits } from 'moment';
+import Favorite from './Favorite'
+
 
 function BlogWithComment(props) {
-    const [liked, setLiked] = useState(false)
-    const [disLiked, setDisLiked] = useState(false)
-
     const comments = props.post.comments.map(comment => <Comment comment={comment} key={comment._id}/>) 
     const commentNumber = props.post.comments.length
-
-    useEffect(() => {
-        console.log('change to likes')
-    }, [setLiked, setDisLiked])
-
-    const handleLike = () => {
-        setLiked(!liked)
-        const num = (liked ? -1 : 1) + props.post.likes 
-
-        fetch(`/api/posts/${props.post._id}/likes`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify({
-                "likes": num,
-            })
-        }).then(res => res.json()).then(data => {
-            console.log(data)
-        })
-    }
-
-    const handleDisLike = () => {
-        setDisLiked(!disLiked) 
-        const num = (disLiked ? -1 : 1) + props.post.dislikes
- 
-        fetch(`/api/posts/${props.post._id}/dislikes`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify({
-                "dislikes": num
-            })
-        }).then(res => res.json()).then(data => {
-            console.log(data)
-        })
-
-    }
  
     return(
         <React.Fragment>
@@ -58,16 +17,13 @@ function BlogWithComment(props) {
                     <h1>{props.post.title}</h1>
                     <p>By: {props.post.author.username}</p> 
                     <p>{props.post.text}</p> 
-                    <div className="likes">
-                        <span><span onClick={handleLike}><FaHeart /> {props.post.likes}</span> <span onClick={handleDisLike}><FaHeartBroken /> {props.post.dislikes}</span> </span>
-                    </div>
+                    <Favorite likes={props.post.likes} dislikes={props.post.dislikes} postId={props.post._id}/>
                 </div>
                 <div className="comments">
                     <h1>Leave a new comment: </h1>
                     <CommentForm postid={props.post._id}/>
                     <h1>{commentNumber === 1 ? `${commentNumber} Comment` : `${commentNumber} Comments`}: </h1>
-                    {comments}
-                    
+                    {comments}      
                 </div>
             </div>
         </React.Fragment>
---
+++ reader-frontend/src/Components/Favorite.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+
+import { FaHeart, FaHeartBroken} from 'react-icons/fa'
+
+
+function Favorite(props) {
+    const [liked, setLiked] = useState(false)
+    const [disLiked, setDisLiked] = useState(false)
+
+    useEffect(() => {
+        console.log('change to likes')
+    }, [setLiked, setDisLiked])
+
+    const handleLike = () => {
+        setLiked(!liked)
+        const num = (liked ? -1 : 1) + props.likes 
+
+        fetch(`/api/posts/${props.postId}/likes`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify({
+                "likes": num,
+            })
+        }).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    }
+
+    const handleDisLike = () => {
+        setDisLiked(!disLiked) 
+        const num = (disLiked ? -1 : 1) + props.dislikes
+ 
+        fetch(`/api/posts/${props.postId}/dislikes`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify({
+                "dislikes": num
+            })
+        }).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+
+    }
+
+    return(
+        <div className="likes">
+            <span>
+                <button onClick={handleLike} disabled={disLiked ? true : false}>
+                    <FaHeart /> {props.likes}
+                </button> 
+                <button onClick={handleDisLike} disabled={liked ? true : false}
+                    ><FaHeartBroken /> {props.dislikes}
+                </button> 
+            </span>
+        </div>
+    )
+}
+
+export default Favorite
