9570e52289c3a1fac192aa408877be2562d231ee
Michaela Wagner <mickywagner@gmail.com>
2020-07-01 16:00:23 -0600

    Add link to github code

--- reader-frontend/src/pages/Home.js
+++ reader-frontend/src/pages/Home.js
@@ -8,7 +8,8 @@ function Home() {
             <div>
                 <h1>Welcome to My Blog!</h1>
                 <p>This is a project I did for Odin Project Node course. It's a REST API built with a Node/Express backend and two front-end sites, both built with React, one for admin (editing, deleting comments/posts) and one for readers. </p>
-                <p><Link to="/blog">Go to Blog <FiArrowRightCircle /> </Link></p>
+                <a href="https://github.com/mickywagner/blog_rest_api" target="_blank">Check out the code on Github</a>
+                <p> <Link to="/blog">Go to Blog <FiArrowRightCircle /> </Link></p>
             </div>  
         </div>
         
