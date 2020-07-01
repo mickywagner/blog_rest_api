001ec78898e0f942ce8e5a905a4d7aad95a6a506
Michaela Wagner <mickywagner@gmail.com>
2020-07-01 15:50:01 -0600

    Add home page intro to blog

--- reader-frontend/src/pages/Home.js
+++ reader-frontend/src/pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import { FiArrowRightCircle } from 'react-icons/fi'
 
 function Home() {
     return(
         <div className="home">
-            <h1>Welcome to my Blog. This is the reader frontend for a Blog API I built using Node/Express backend and React frontend.</h1>
+            <div>
+                <h1>Welcome to My Blog!</h1>
+                <p>This is a project I did for Odin Project Node course. It's a REST API built with a Node/Express backend and two front-end sites, both built with React, one for admin (editing, deleting comments/posts) and one for readers. </p>
+                <p><Link to="/blog">Go to Blog <FiArrowRightCircle /> </Link></p>
+            </div>  
         </div>
         
     )
 }
 
 export default Home
+
