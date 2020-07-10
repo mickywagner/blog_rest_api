6bc638a376457a2aba6e0d2a8b0941b617cd2139
Michaela Wagner <mickywagner@gmail.com>
2020-07-09 18:44:04 -0600

    Add whitelist urls and functon for cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,23 +3,29 @@ import 'dotenv/config'
 import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
-
-
 import { connectDb } from './models'
 import indexRouter from './routes/index'
 import apiRouter from './routes/api'
-
 import cookieParser from 'cookie-parser'
-
 
 const app = express()
 
+let whitelist = ['http://localhost:3000', 'https://mickywagner.github.io/blog_rest_api/', 'https://blog-admin-2020.netlify.app/']
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ orign: [
-    'https://mickywagner.github.io/blog_rest_api/',
-    'https://blog-admin-2020.netlify.app/'
-]}))
+app.use(cors({
+    origin: function(origin, callback){
+      if(!origin) return callback(null, true);
+      if(whitelist.indexOf(origin) === -1){
+        var message = `The CORS policy for this origin doesn't ' +
+                  'allow access from the particular origin.`;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  }));
 app.use(cookieParser())
 mongoose.set('useFindAndModify', false);
 
